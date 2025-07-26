93b9828b4861d4499c328ae12c419ab0a8316325
Jabrial Gentry <farpoint@wgu.edu>
2025-07-26 17:41:55 -0600

    Add files via upload

---
+++ invoice_pwa_assets/service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('invoice-cache').then(cache => {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './icon-192.png',
+        './icon-512.png',
+        './service-worker.js'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
